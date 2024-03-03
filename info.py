0b3d66b1b8e38c7bf11b2a324a6ca6fd59078d09
Praveen Kumar K <125640021+shinchan2627@users.noreply.github.com>
2024-03-03 09:58:13 +0530

    Update info.py

--- info.py
+++ info.py
@@ -41,7 +41,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002029516760'))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/tamcinemas')
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/+0TBKVN0Z6yM1MTc1')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), True)
 IMDB = is_enabled((environ.get('IMDB', "True")), False)
 SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "True")), False)
